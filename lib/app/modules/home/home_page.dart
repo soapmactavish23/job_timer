0ad068350f84986366f572ed35db49419abd2abe
soapmactavish23 <ricknogueira1231@gmail.com>
2024-06-02 19:18:59 -0300

    Aula 1 finalizada

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Home'),
       ),
       body: Container(),
     );
--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -5,11 +5,41 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF0092B9),
+              Color(0xFF0167B2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+              ),
+              SizedBox(height: screenSize.height * .1),
+              SizedBox(
+                width: screenSize.width * .8,
+                height: 49,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                  ),
+                  child: Image.asset('assets/images/google.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
